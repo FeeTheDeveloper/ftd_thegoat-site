c6837dda9a91f8ef4a0cc32a0946d64d87c84032
Fee The Developer <fee@feethedeveloper.com>
2026-01-24 07:27:45 -0600

    Make header fade transparent on scroll

--- script.js
+++ script.js
@@ -16,3 +16,18 @@ revealItems.forEach(item => {
     item.classList.add('reveal');
     observer.observe(item);
 });
+
+const header = document.querySelector('.site-header');
+const updateHeaderTransparency = () => {
+    if (!header) {
+        return;
+    }
+
+    const shouldBeTransparent = window.scrollY > 40;
+    header.classList.toggle('is-transparent', shouldBeTransparent);
+};
+
+updateHeaderTransparency();
+window.addEventListener('scroll', () => {
+    window.requestAnimationFrame(updateHeaderTransparency);
+});
